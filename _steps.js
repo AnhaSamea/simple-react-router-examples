b922f73cf342537a8e5bc3f080edafbae321df47
anhasamea <anhasamea707@gmail.com>
2022-10-07 18:30:27 +0600

    route with links

--- _steps.js
+++ _steps.js
@@ -4,3 +4,9 @@
 3. Add RouterProvider and pass router props
 4. Create some route inside the router
 */
+/* 
+Things you need to create a route:
+1. Create a Link : so that you can go this route
+2. Create a component: to add what you will show once you go to that route
+3. Add route so that react router knows the component it needs to display while you are visiting to that route
+ */
