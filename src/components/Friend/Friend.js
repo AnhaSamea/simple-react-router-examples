65c0211beba6fb18c665f628ceff86a687db8a8d
anhasamea <anhasamea707@gmail.com>
2022-10-08 10:34:19 +0600

    dynamic link

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link } from 'react-router-dom';
 import './Friend.css'
 const Friend = ({friend}) => {
-    const {name,username,email}=friend;
+    const {id,name,username,email}=friend;
 
     return (
         <div className='friend'>
             <h2>{name}</h2>
-            <p><strong>User name:</strong> {username}</p>
+            <p><strong>User name:</strong><Link to={`/friend/${id}`}>{username}</Link></p>
             <p><strong>Email:</strong> {email}</p>
         </div>
     );
