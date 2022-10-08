dd053a9d3cbbd6ffa62643dd3e3104bb712fa706
anhasamea <anhasamea707@gmail.com>
2022-10-08 10:27:32 +0600

    loaded data dynamicly

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './Friend.css'
 const Friend = ({friend}) => {
     const {name,username,email}=friend;
 
     return (
-        <div>
+        <div className='friend'>
             <h2>{name}</h2>
             <p><strong>User name:</strong> {username}</p>
             <p><strong>Email:</strong> {email}</p>
